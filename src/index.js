2729fee347f457c7b4bf6bf9fe826b9bdfad6444
liujunjie <liujunjie@bbdservice.com>
2020-05-06 15:42:23 +0800

    init

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import 'systemjs/dist/system';
+import 'systemjs/dist/extras/global';
+import 'systemjs/dist/extras/named-exports';
+import './system-css-extra';
---
+++ src/system-css-extra.js
@@ -0,0 +1,43 @@
+'use strict';
+
+(function() {
+    var systemPrototype = System.constructor.prototype;
+    var originalInstantiate = systemPrototype.instantiate;
+
+    systemPrototype.instantiate = function() {
+        var url = arguments[0];
+
+        if (url.slice(-4) === '.css') {
+            return new Promise(function(resolve, reject) {
+                if (document.querySelector('link[href="' + url + '"]') || document.querySelector('link[href="' + url.replace(location.protocol + '//' + location.hostname, '') + '"]')) {
+                    reject(Error('Style ' + url + ' has already been loaded using another way.'));
+                    return;
+                }
+
+                var link = document.createElement('link');
+                link.type = 'text/css';
+                link.rel = 'stylesheet';
+                link.href = url;
+                document.head.appendChild(link);
+
+                link.onload = function() {
+                    resolve([[], function() {
+                        return {};
+                    }]);
+                };
+
+                link.onerror = function(e) {
+                    var href = document.querySelector('link[href="' + url + '"]');
+
+                    if (href) {
+                        href.parentElement.removeChild(href);
+                    }
+
+                    reject(e);
+                };
+            });
+        } 
+        return originalInstantiate.apply(this, arguments);
+    
+    };
+})();
